ca84487f419f64a619d7ad1a4a77b710610c2910
Aanand Kumar <aanandmrh222@gmail.com>
2024-06-13 21:31:50 +0530

    java codes

---
+++ Multithreding/ak_07practiceSet/Main.java
@@ -0,0 +1,12 @@
+package Multithreding.ak_07practiceSet;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadState t1 = new ThreadState();
+        System.out.printf("\nCreate the thread %s",t1.getState());
+        t1.start();
+        t1.join();
+        System.out.printf("\nThread finished %s",t1.getState());
+
+    }
+}
---
+++ Multithreding/ak_07practiceSet/ThreadState.java
@@ -0,0 +1,14 @@
+package Multithreding.ak_07practiceSet;
+
+public class ThreadState extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(4000);
+            System.out.printf("\nFrom inside run %s", getState());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
